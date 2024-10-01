e772b60fcbb41b83dd06400ca3a13466071f3326
joplus2 <149871843+joplus2@users.noreply.github.com>
2024-10-01 20:20:56 +0200

    Update CamShoot.py
    Deleted redundant output definition and added out definition from io config via import 
    Added display line showing how many photos are left

--- project/cam/CamShoot.py
+++ project/cam/CamShoot.py
@@ -1,19 +1,22 @@
 # část kódu pro focení
 import time
-from machine import Pin
+from disp import vga_16x32
+from config.io import *
 
-pinOut = Pin(23, Pin.OUT)
 
-def ShootFun(Shoot, TimeShoot, TimeBtween, ShootCount):
-    global pinOut
+def ShootFun(Shoot, TimeShoot, TimeBtween, ShootCount, tft):
     while ( Shoot == True ):
-        pinOut.on()
+        string = 'Zbyva: ' + str((ShootCount-1))
+        tft.text(vga_16x32, string, 10, 40)
+        outCamShoot.on()
         time.sleep(TimeShoot)
-        pinOut.off()
+        outCamShoot.off()
         time.sleep(TimeBtween)
         if ( ShootCount > 1):
             ShootCount -= 1
         else:
+            string = '                       '
+            tft.text(vga_16x32, string, 10, 40)
             Shoot = False
     return
 
