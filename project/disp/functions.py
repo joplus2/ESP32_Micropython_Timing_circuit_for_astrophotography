5552be68ad2b17a013da9bef83173634fb3cb5d2
joplus2 <149871843+joplus2@users.noreply.github.com>
2024-11-13 22:21:10 +0100

    Created functions.py and added display funcs
    Created file and added functions to refresh and/or display parts.

---
+++ project/disp/functions.py
@@ -0,0 +1,61 @@
+# file with display functions
+from disp import vga_16x32
+
+# initial vars state
+PrevString = ""
+PrevTimeShoot = 0
+PrevTimeBtween = 0
+PrevShootCount = 0
+
+def showSecondBtns(tft):
+    tft.text(vga_16x32, '^', 70, 5)
+    tft.text(vga_16x32, 'v', 70, 200)
+    tft.text(vga_16x32, '+', 70, 15)
+    tft.text(vga_16x32, 's', 70, 40)
+    
+def showDecimalBtns(tft):
+    tft.text(vga_16x32, '^', 240, 5)
+    tft.text(vga_16x32, 'v', 240, 200)
+    tft.text(vga_16x32, '+', 240, 15)
+    tft.text(vga_16x32, 's/10', 220, 40)
+    
+def vanishSecBtns(tft):
+    tft.text(vga_16x32, '  ', 70, 5)
+    tft.text(vga_16x32, '  ', 70, 200)
+    tft.text(vga_16x32, '  ', 70, 15)
+    tft.text(vga_16x32, '  ', 70, 40)
+    
+def vanishDecBtns(tft):
+    tft.text(vga_16x32, '  ', 240, 5)
+    tft.text(vga_16x32, '  ', 240, 200)
+    tft.text(vga_16x32, '  ', 240, 15)
+    tft.text(vga_16x32, '    ', 220, 40)
+    
+def updateString(tft, String):
+    global PrevString
+    if ( PrevString != String ):
+        # zapsat nový string
+        tft.text(vga_16x32, String, 10, 100)
+        PrevString = String
+    
+def updateTimeShoot(tft, TimeShoot):
+    global PrevTimeShoot
+    if ( PrevTimeShoot != TimeShoot ):
+        # zapsat nový čas focení
+        tft.text(vga_16x32, str(TimeShoot) + " s   ", 30, 170)
+        PrevTimeShoot = TimeShoot
+        
+def updateTimeBtween(tft, TimeBtween):
+    global PrevTimeBtween
+    if ( PrevTimeBtween != TimeBtween ):
+        # zapsat nový čas mezi fotkami
+        tft.text(vga_16x32, str(TimeBtween) + " s   ", 150, 170)
+        PrevTimeBtween = TimeBtween
+        
+def updateShootCount(tft, ShootCount):
+    global PrevShootCount
+    if ( PrevShootCount != ShootCount ):
+        # zapsat nový počet snímků
+        tft.text(vga_16x32, str(ShootCount) + " x   ", 260, 170)
+        PrevShootCount = ShootCount
+        
