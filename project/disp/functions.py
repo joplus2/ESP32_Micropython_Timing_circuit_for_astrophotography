0afc9f7ee23e98268a6baa7f05056de555ced9b7
joplus2 <149871843+joplus2@users.noreply.github.com>
2024-12-12 18:58:40 +0100

    Update functions.py
    Added function to show warning when exceeding maximal recomended time between shots

--- project/disp/functions.py
+++ project/disp/functions.py
@@ -1,5 +1,7 @@
 # file with display functions
 from disp import vga_16x32
+from disp import st7789
+
 
 # initial vars state
 PrevString = ""
@@ -11,25 +13,25 @@ def showSecondBtns(tft):
     tft.text(vga_16x32, '^', 70, 5)
     tft.text(vga_16x32, 'v', 70, 200)
     tft.text(vga_16x32, '+', 70, 15)
-    tft.text(vga_16x32, 's', 70, 40)
+    #tft.text(vga_16x32, 's', 70, 40)
     
 def showDecimalBtns(tft):
     tft.text(vga_16x32, '^', 240, 5)
     tft.text(vga_16x32, 'v', 240, 200)
     tft.text(vga_16x32, '+', 240, 15)
-    tft.text(vga_16x32, 's/10', 220, 40)
+    #tft.text(vga_16x32, 's/10', 220, 40)
     
 def vanishSecBtns(tft):
     tft.text(vga_16x32, '  ', 70, 5)
     tft.text(vga_16x32, '  ', 70, 200)
     tft.text(vga_16x32, '  ', 70, 15)
-    tft.text(vga_16x32, '  ', 70, 40)
+    #tft.text(vga_16x32, '  ', 70, 40)
     
 def vanishDecBtns(tft):
     tft.text(vga_16x32, '  ', 240, 5)
     tft.text(vga_16x32, '  ', 240, 200)
     tft.text(vga_16x32, '  ', 240, 15)
-    tft.text(vga_16x32, '    ', 220, 40)
+    #tft.text(vga_16x32, '    ', 220, 40)
     
 def updateString(tft, String):
     global PrevString
@@ -58,4 +60,11 @@ def updateShootCount(tft, ShootCount):
         # zapsat nový počet snímků
         tft.text(vga_16x32, str(ShootCount) + " x   ", 260, 170)
         PrevShootCount = ShootCount
+        
+def showTimeWarning(tft, show=True):
+    if ( show ):
+        # show warning, that recomended time was exceeded
+        tft.text(vga_16x32, "Presazen dop. cas!", 10, 60, background=st7789.RED)
+    else:
+        tft.fill_rect(10, 60, 300, 32, st7789.BLACK)
         
