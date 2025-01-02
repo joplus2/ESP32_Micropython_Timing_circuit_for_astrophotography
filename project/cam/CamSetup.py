1170832b8fb0132456dc11259c197dd40db34003
joplus2 <149871843+joplus2@users.noreply.github.com>
2025-01-02 12:03:53 +0100

    Update CamSetup.py

--- project/cam/CamSetup.py
+++ project/cam/CamSetup.py
@@ -1,6 +1,6 @@
 # část kódu pro nastavení parametrů
 import time
-from machine import Pin, SPI
+from machine import Pin
 from disp import st7789
 from disp import vga_16x32
 from config.io import *
@@ -8,7 +8,7 @@ from disp.functions import *
 
 
 def SetupFun(Setup, TimeShoot, TimeBtween, ShootCount, tft):
-    # definice základního nastavení
+    # definition of base settings
     Step = 0
     TempStep = 1
     ChangeParams = True
@@ -18,13 +18,13 @@ def SetupFun(Setup, TimeShoot, TimeBtween, ShootCount, tft):
     PrevTimeShoot = 0
     PrevTimeBtween = 0
     PrevShootCount = 0
-    # zobrazeni popisu vterin
+    # show seconds button description
     showSecondBtns(tft)
     
     while ( Setup == True ):
        if ( Step == 0 ):
            if ( TimeShoot > 0 ) and ( TimeBtween > 0 ) and ( ShootCount > 0 ):
-               # zeptat se zda změnit parametry
+               # ask if parameters want to be changed
                if ( btnUpS.value() == True ) or ( btnDwnS.value() == True ):
                    ChangeParams = not (ChangeParams)
                    time.sleep(0.5)
@@ -40,6 +40,7 @@ def SetupFun(Setup, TimeShoot, TimeBtween, ShootCount, tft):
                    time.sleep(0.5)
            else:
                Step = 1 
+               
        elif ( Step == 1 ):
            # set up first parameter - shooting time
            if ( btnUpS.value() == True ):
@@ -62,14 +63,6 @@ def SetupFun(Setup, TimeShoot, TimeBtween, ShootCount, tft):
                else:
                    TimeShoot -= 60
                time.sleep(0.5)
-                   
-           #if ( btnUpDec.value() == True ):
-           #    TimeShoot += 0.1
-           #    time.sleep(0.5)  
-           #if ( btnDwnDec.value() == True ) and ( TimeShoot > 0 ):
-           #    TimeShoot -= 0.1
-           #    time.sleep(0.5)
-               
            if ( btnEnter.value() == True ) and ( TimeShoot > 0):
                time.sleep(0.5)
                Step = 2
@@ -77,38 +70,30 @@ def SetupFun(Setup, TimeShoot, TimeBtween, ShootCount, tft):
                String = "Cas zaverky: " + str(int(TimeShoot/60)) + " min        "
            else:
                String = "Cas zaverky: " + str(TimeShoot) + " s        " 
+               
        elif ( Step == 2 ):
            # set up second parameter - time between shots
            if ( btnUpS.value() == True ):
                TimeBtween += 0.1
                time.sleep(0.5)
-                   
            if ( btnDwnS.value() == True ) and ( TimeBtween > 0 ):
                TimeBtween -= 0.1
                time.sleep(0.5)
-                   
            TimeBtween = round(TimeBtween,1)
-           #if ( btnUpDec.value() == True ):
-           #    TimeBtween += 0.1
-           #    time.sleep(0.5)  
-           #if ( btnDwnDec.value() == True ) and ( TimeBtween > 0 ):
-           #    TimeBtween -= 0.1
-           #    time.sleep(0.5)  
            if ( btnEnter.value() == True ) and ( TimeBtween > 0 ):
                time.sleep(0.5)
-               #vanishDecBtns(tft)
                Step = 3
            if (TimeBtween > 1 ):
                showTimeWarning(tft)
            else:
                showTimeWarning(tft, False)
            String = "Cas mezi sn.: " + str(TimeBtween) + " s       " 
+           
        elif ( Step == 3 ):
-           # nastavíme počet fotek
+           # set up third parameter - count of shots
            if ( btnUpS.value() == True ):
                ShootCount += 1
                time.sleep(0.5)
-                   
            if ( btnDwnS.value() == True ) and ( ShootCount >= 1 ):
                ShootCount -= 1
                time.sleep(0.5)  
@@ -117,24 +102,25 @@ def SetupFun(Setup, TimeShoot, TimeBtween, ShootCount, tft):
                time.sleep(0.5)
                Step = 4
            String = "Pocet snimku: " + str(ShootCount) + "        "   
+           
        elif (Step == 4 ):
-           # zrevidujeme časy
+           # revision of parameters
            if ( btnUpS.value() == True ):
                if ( TempStep < 4 ):
-                   TempStep += 1                           # inkrementujeme krok do limitu 4
+                   TempStep += 1                           # increment step number to limit of 4
                else:
-                   TempStep = 1                            # při pokusu inkrementovat nad dáme 1
+                   TempStep = 1                            # if increment above, step 1
                time.sleep(0.5)
                
            if ( btnDwnS.value() == True ):
                if ( TempStep <= 1 ):
-                   TempStep = 4                            # při pokusu dekrementovat krok pod limit 1 dáme 4
+                   TempStep = 4                            # if decrement under 1 step 4
                else:
-                   TempStep -= 1                            # dekrementujeme jinak krok
+                   TempStep -= 1                            # decrement step number
                time.sleep(0.5)
                
            if ( TempStep == 1 ):
-               String = 'Nast. cas zaverky        '         # přiřadíme texty
+               String = 'Nast. cas zaverky        '         # selection of display text
            elif ( TempStep == 2 ):
                String = 'Nast. cas mezi sn.       '
            elif ( TempStep == 3 ):
@@ -146,12 +132,11 @@ def SetupFun(Setup, TimeShoot, TimeBtween, ShootCount, tft):
                if ( TempStep == 4 ):
                    Step = 5
                else:
-                   #if TempStep == 1 or TempStep == 2:
-                   #    showDecimalBtns(tft)
                    Step = TempStep
                time.sleep(0.5)
+               
        elif ( Step == 5 ):
-           # zeptat se zda rovnou fotit
+           # ask if start photo shoot
            if ( btnUpS.value() == True ) or ( btnDwnS.value() == True):
                ShootNow = not( ShootNow )
                time.sleep(0.5)
@@ -180,8 +165,6 @@ def SetupFun(Setup, TimeShoot, TimeBtween, ShootCount, tft):
     
     # smazani popisu tlacitek
     # Vterinova tlacitka
-    vanishSecBtns(tft)
-    # desetinna tlacitka
     vanishSecBtns(tft)
     
     ToReturn = [TimeShoot, TimeBtween, ShootCount, Shoot]
