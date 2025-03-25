7b65dd1434c4a453572cf9e81655be53e8d87a61
joplus2 <149871843+joplus2@users.noreply.github.com>
2025-03-25 20:03:16 +0100

    Update CamShoot.py
    Made external functions for shooting

--- project/cam/CamShoot.py
+++ project/cam/CamShoot.py
@@ -2,15 +2,20 @@
 import time
 from disp import vga_16x32
 from config.io import *
+from cam.functions import *
 
 
-def ShootFun(Shoot, TimeShoot, TimeBtween, ShootCount, tft):
+def ShootFun(Shoot, TimeShoot, TimeBtween, ShootCount, tft, ptpip, wlanActive):
     while ( Shoot == True ):
         string = 'Zbyva: ' + str((ShootCount-1)) + '    '
         tft.text(vga_16x32, string, 10, 20)
-        outCamShoot.on()
-        time.sleep(TimeShoot)
-        outCamShoot.off()
+        #outCamShoot.on()
+        #time.sleep(TimeShoot)
+        #outCamShoot.off()
+        if ( wlanActive == True ):
+            ptpipShutter(ptpip)
+        else:
+            wiredShutter(TimeShoot)
         time.sleep(TimeBtween)
         if ( ShootCount > 1):
             ShootCount -= 1
