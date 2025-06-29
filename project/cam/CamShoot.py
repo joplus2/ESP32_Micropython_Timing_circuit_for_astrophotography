29045925f1234df531f7458749a5cf26a43d202f
joplus2 <149871843+joplus2@users.noreply.github.com>
2025-06-29 11:17:14 +0200

    Update CamShoot.py

--- project/cam/CamShoot.py
+++ project/cam/CamShoot.py
@@ -1,17 +1,16 @@
-# část kódu pro focení
+# shooting function
 import time
 from disp import vga_16x32
 from config.io import *
 from cam.functions import *
+from config.battery import battMeasure
 
 
 def ShootFun(Shoot, TimeShoot, TimeBtween, ShootCount, tft, ptpip, wlanActive):
     while ( Shoot == True ):
         string = 'Zbyva: ' + str((ShootCount-1)) + '    '
         tft.text(vga_16x32, string, 10, 20)
-        #outCamShoot.on()
-        #time.sleep(TimeShoot)
-        #outCamShoot.off()
+        battMeasure(tft)
         if ( wlanActive == True ):
             ptpipShutter(ptpip, TimeShoot)
         else:
